b7c60878c506d6185bce1b40d8c1171fe0cb585c
Weixiang Wang <43213773+Lidweixiang@users.noreply.github.com>
2025-10-27 12:30:41 +0800

    Add files via upload

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,3 +4,9 @@ echo "installing MonSTR-Bayes..."
 cd MonSTR-Bayes/
 pip install python/
 echo "MonSTR-Bayes installed"
+
+# install MoSTR-Bulk
+echo "installing MoSTR-Bulk..."
+cd ../
+conda env create -f MoSTR-Bulk/environment.yml
+echo "MoSTR-Bulk installed"
